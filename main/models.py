12ba05a0a623e575907554efeb0593c71397c873
PythonShinobi <pythonshinobi@gmail.com>
2024-02-28 20:27:44 +0300

    Update import statement from . import db to from main import db.

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 
-from . import db
+from main import db
 
 class Anime(db.Model):
     __tablename__ = 'anime_model'
