b1c7072d949ab2085397bae62e0dacf6a69c6fe0
PythonShinobi <pythonshinobi@gmail.com>
2024-02-28 10:36:18 +0300

    Add configuration settings using environment variables.

---
+++ config.py
@@ -0,0 +1,13 @@
+
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+env = '.env'
+load_dotenv(env)
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+        f'sqlite:///{os.path.join(basedir, "app.db")}'
